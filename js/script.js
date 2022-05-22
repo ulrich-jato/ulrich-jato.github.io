a5e9706197bee1022726a426a1c4b64cd62bf16e
ulrich-jato <ulrichjato@yahoo.fr>
2022-05-22 11:36:11 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======================== typing animation ====================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "Web developer", "Graphic Designer", "Full stack developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
